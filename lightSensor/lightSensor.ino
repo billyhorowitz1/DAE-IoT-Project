d791a22286f7fb46f3e126ed074874caf7596929
billy_horowitz <whorowitz2@gmail.com>
2023-12-20 14:19:02 -0500

    Added LED that dims when the room gets darker

--- lightSensor/lightSensor.ino
+++ lightSensor/lightSensor.ino
@@ -1,17 +1,31 @@
 // Configuration variables
 const int sensorPin = A0;
+const int ledPin = 6;
 
 // Setup variables and serial communication
 void setup() {
   
   Serial.begin(9600);
   pinMode(sensorPin, INPUT);
+  pinMode(ledPin, OUTPUT);
 
 }
 
 // Main loop
 void loop() {
 
+  int sensorValue = analogRead(sensorPin);
   Serial.println(analogRead(sensorPin));
+
+  analogWrite(ledPin, map(sensorValue, 300, 1000, 0, 100));
+  // if (sensorValue < 500) {
+
+  //     digitalWrite(ledPin, HIGH);
+
+  // } else {
+
+  //     digitalWrite(ledPin, LOW);
+
+  // }
 
 }
